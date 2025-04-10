b3091adcf87bf2e00f21d0a1b2ddc0dfc1efdbbf
Mohamed-samy2 <mohamedsamyy02@gmail.com>
2025-04-10 14:17:25 +0200

    resolve conflict

--- prvia-frontend-app/src/utilities/constants.js
+++
@@ -1,6 +0,0 @@
-// src/utils/constants.js
-export const DEFAULT_STATUS = 'PENDING';
-export const UPDATED_STATUS = 'PASSED';
-export const ACCEPTED_STATUS = 'ACCEPTED';
-export const REJECTED_STATUS = 'REJECTED';
-export const INTERVIEW_PROCESS_STATUS = 'INTERVIEW-PROCESS';
