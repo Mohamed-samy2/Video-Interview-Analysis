b556cbb109765a2bfbadb00d02bdeb20cebc72b1
Yomna Mohammed Bassam <133790789+yomnamuhammedd@users.noreply.github.com>
2025-05-11 15:52:13 +0300

    removed extra comments and added imports

--- prvia-frontend-app/src/index.js
+++ prvia-frontend-app/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
+import './styles/NavbarStyles.css';
+import './styles/LoginSignup.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
