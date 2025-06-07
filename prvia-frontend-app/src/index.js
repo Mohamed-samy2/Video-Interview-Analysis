d04a3009549e224b35832e72e0872caefa336c9d
Yomna Mohammed Bassam <133790789+yomnamuhammedd@users.noreply.github.com>
2025-06-08 00:35:51 +0300

    feat: Style Job Details Page and Refactor Components.This commit introduces styling for the Job Details page to improve its visual presentation and user experience. It also refactors the JobDetails components in both the user and HR sections to utilize the new styling and improve code structure.

--- prvia-frontend-app/src/index.js
+++ prvia-frontend-app/src/index.js
@@ -3,6 +3,7 @@ import App from './App';
 import './styles/global.css';
 import './styles/NavbarStyles.css';
 import './styles/LoginSignup.css';
+import './styles/JobDetails.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
