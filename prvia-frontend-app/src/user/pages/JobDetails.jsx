cb0c96a94f4f485ca858e6d3a09dd774b8285594
Yomna Mohammed Bassam <133790789+yomnamuhammedd@users.noreply.github.com>
2025-06-08 22:32:30 +0300

    removed un used import

--- prvia-frontend-app/src/user/pages/JobDetails.jsx
+++ prvia-frontend-app/src/user/pages/JobDetails.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
-import { Container, Card, Button, Spinner } from 'react-bootstrap';
+import { Container, Button, Spinner } from 'react-bootstrap';
 import { getJobById } from '../../services/api';
 import { toast } from 'react-toastify';
 
